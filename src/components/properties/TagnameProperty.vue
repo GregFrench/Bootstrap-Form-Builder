4fd7a27e301610b486b9748c385405fd48d813ed
Greg French <gregfrench26@gmail.com>
2020-07-15 02:08:19 -0400

    Cleaned up spacing in the TagnameProperty.vue component

--- src/components/properties/TagnameProperty.vue
+++ src/components/properties/TagnameProperty.vue
@@ -1,31 +1,26 @@
 <template>
   <div class="form-group">
-      <label>Heading Size</label>
-      <div class="radio-wrapper">
-          <label
-            v-bind:class="{
-              'label-active': tagName === null || tagName === 'h1'
-            }"
-            class="radio-inline"
-          >
-              <input type="radio" name="optradio" v-model="tagName" value="h1">H1
-          </label>
-          <label v-bind:class="{ 'label-active': tagName === 'h2' }" class="radio-inline">
-              <input type="radio" name="optradio" v-model="tagName" value="h2">H2
-          </label>
-          <label v-bind:class="{ 'label-active': tagName === 'h3' }" class="radio-inline">
-              <input type="radio" name="optradio" v-model="tagName" value="h3">H3
-          </label>
-          <label v-bind:class="{ 'label-active': tagName === 'h4' }" class="radio-inline">
-              <input type="radio" name="optradio" v-model="tagName" value="h4">H4
-          </label>
-          <label v-bind:class="{ 'label-active': tagName === 'h5' }" class="radio-inline">
-              <input type="radio" name="optradio" v-model="tagName" value="h5">H5
-          </label>
-          <label v-bind:class="{ 'label-active': tagName === 'h6' }" class="radio-inline">
-              <input type="radio" name="optradio" v-model="tagName" value="h6">H6
-          </label>
-      </div>
+    <label>Heading Size</label>
+    <div class="radio-wrapper">
+      <label v-bind:class="{ 'label-active': tagName === 'h1' }" class="radio-inline">
+        <input type="radio" name="optradio" v-model="tagName" value="h1">H1
+      </label>
+      <label v-bind:class="{ 'label-active': tagName === 'h2' }" class="radio-inline">
+        <input type="radio" name="optradio" v-model="tagName" value="h2">H2
+      </label>
+      <label v-bind:class="{ 'label-active': tagName === 'h3' }" class="radio-inline">
+        <input type="radio" name="optradio" v-model="tagName" value="h3">H3
+      </label>
+      <label v-bind:class="{ 'label-active': tagName === 'h4' }" class="radio-inline">
+        <input type="radio" name="optradio" v-model="tagName" value="h4">H4
+      </label>
+      <label v-bind:class="{ 'label-active': tagName === 'h5' }" class="radio-inline">
+        <input type="radio" name="optradio" v-model="tagName" value="h5">H5
+      </label>
+      <label v-bind:class="{ 'label-active': tagName === 'h6' }" class="radio-inline">
+        <input type="radio" name="optradio" v-model="tagName" value="h6">H6
+      </label>
+    </div>
   </div>
 </template>
 
